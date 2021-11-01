9eddfe259ef0e9470d00cc4d0fd5398dbf0cd0bf
hitman <mrezaee79@gmail.com>
2021-11-01 07:16:12 -0700

    add new pages in sidebar menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      v-model='drawer'
-      :mini-variant='miniVariant'
-      :clipped='clipped'
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
       fixed
       app
     >
       <v-list>
         <v-list-item
-          v-for='(item, i) in items'
-          :key='i'
-          :to='item.to'
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
           router
           exact
         >
@@ -19,41 +19,25 @@
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text='item.title' />
+            <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left='clipped'
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop='drawer = !drawer' />
-      <v-btn
-        icon
-        @click.stop='miniVariant = !miniVariant'
-      >
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop='clipped = !clipped'
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop='fixed = !fixed'
-      >
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
-      <v-toolbar-title v-text='title' />
+      <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop='rightDrawer = !rightDrawer'
-      >
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
@@ -62,33 +46,23 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model='rightDrawer'
-      :right='right'
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
-        <v-list-item @click.native='right = !right'>
+        <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute='!fixed'
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -101,19 +75,29 @@ export default Vue.extend({
         {
           icon: 'mdi-apps',
           title: 'Welcome',
-          to: '/'
+          to: '/',
         },
         {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
-          to: '/inspire'
-        }
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-newspaper',
+          title: 'Posts',
+          to: '/posts',
+        },
+        {
+          icon: 'mdi-file-plus',
+          title: 'Create Post',
+          to: '/insertPost',
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Vuetify.js',
     }
-  }
+  },
 })
 </script>
